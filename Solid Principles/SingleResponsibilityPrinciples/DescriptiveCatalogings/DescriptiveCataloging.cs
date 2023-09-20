7c248025f674e31f5f78cad90e60036acbce82af
Bryan Jerahmeel Yao <bgyao@live.com>
2023-09-20 10:03:54 +0800

    Single Responsibility Principle
    This principle states that "A class should have only one reason to change."
    
    In layman terminology, this means that a class should not be loaded with multiple responsibilities and a single responsibility should not be spread across multiple classes or mixed with other responsibilities. The reason is that more changes requested in the future, the more changes the class need to apply.
    
    Reference: DotNetTricks. SOLID Design Principles Explained - C#. Retrieved from https://www.dotnettricks.com/learn/designpatterns/solid-design-principles-explained-using-csharp on September 20, 2023

---
+++ Solid Principles/SingleResponsibilityPrinciples/DescriptiveCatalogings/DescriptiveCataloging.cs
@@ -0,0 +1,39 @@
+﻿namespace Solid_Principles.SingleResponsibilityPrinciples.DescriptiveCatalogings;
+
+public class BookDetails
+{
+    public Guid id = new Guid();
+    public string bookTitle;
+    public string? bookSubtitle;
+    public string? edition;
+
+    public void PrintBookBasicDetails()
+    {
+        var displayDetails = $"This book is titled {bookTitle}";
+
+        if (!string.IsNullOrEmpty(bookSubtitle))
+        {
+            displayDetails = string.Join(" : ", displayDetails, bookSubtitle);
+        }
+
+        displayDetails = string.Join(", ", displayDetails, edition);
+        Console.WriteLine(displayDetails);
+    }
+ 
+}
+
+public class Author
+{
+    public Guid AuthorId = new Guid();
+    public string FirstName { get; set; }
+    public string SecondName { get; set; }
+    public string FullName { get { return $"{FirstName} {SecondName}"; } }
+    public string FullNameInverse { get { return $"{SecondName}, {FirstName}"; }  }
+    public bool isMainEntry { get; set; }
+    public int entryType { get; set; }
+
+    public void AuthorNotice()
+    {
+        Console.WriteLine("This class is responsible for processing the resource's Statement of Responsibility Personal Name.");
+    }
+}
