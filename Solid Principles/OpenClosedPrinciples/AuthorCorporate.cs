11427f057e277397dd60af71fca7b7d5cdcebe2a
Bryan Jerahmeel Yao <bgyao@live.com>
2023-09-21 22:32:29 +0800

    Open-Closed Principle fix
    Implemented OCP based on CSharpCorner* but using Main Entry--Personal Name and Main Entry--Corporate Name** as examples
    
    *https://www.c-sharpcorner.com/UploadFile/damubetha/solid-principles-in-C-Sharp/
    **https://www.oclc.org/bibformats/en/1xx/100.html
    **https://www.oclc.org/bibformats/en/1xx/110.html

--- Solid Principles/OpenClosedPrinciples/AuthorCorporate.cs
+++ Solid Principles/OpenClosedPrinciples/AuthorCorporate.cs
@@ -1,19 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Solid_Principles.SingleResponsibilityPrinciples.DescriptiveCatalogings;
+﻿namespace Solid_Principles.OpenClosedPrinciples;
 
-namespace Solid_Principles.OpenClosedPrinciples;
-
-public class AuthorCorporate
+public class AuthorCorporate : Author
 {
-    public Author Author { get; set; } = new();
-    public int MarcCode { get; set; }
+    public override int MarcCode { get => base.MarcCode; set => base.MarcCode = isMainEntry ? 110 : 710; }
+    public override string FullName => base.FullName = FirstName;
 
-    public async Task <Author> SetCorporateEntityAsAuthor (int entryType, string corporateName, bool isMainEntry)
+
+    public override void SetEntityAsAuthor (int entryType, string corporateName, bool isMainEntry)
     {
+        MarcCode = isMainEntry ? 110 : 710;
+
         if (entryType == 0)
             Console.WriteLine("Inverted name of the Corporate entity will be used");
         else if (entryType == 1)
@@ -22,20 +18,5 @@ public class AuthorCorporate
             Console.WriteLine("Name in direct order of the Corporate entity will be used");
         else
             Console.WriteLine("NOTICE: entryType is undefined. Default # will be used.");
-
-        if (isMainEntry)
-            MarcCode = 110;
-        else
-            MarcCode = 710;
-
-        Author = new Author()
-        {
-            entryType = entryType,
-            FirstName = corporateName,
-            SecondName = " ",
-            isMainEntry = isMainEntry,
-        };
-
-        return Author;
     }
 }
---
+++ Solid Principles/OpenClosedPrinciples/StatementOfResponsibility.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_Principles.OpenClosedPrinciples;
+
+public class StatementOfResponsibility
+{
+    public StatementOfResponsibility()
+    {
+
+    }
+    AuthorCorporate authorCorporate { get; set; }
+    AuthorPersonal authorPersonal { get; set; }
+
+    public async Task <Author> SetPersonalNameAsAuthor()
+    {
+        authorPersonal = new()
+        {
+            isMainEntry = true,
+            entryType = 1,
+            SecondName = "Ibarra",
+            FirstName = "Crisostomo"
+        };
+
+        return authorPersonal;
+    }
+
+    public async Task <Author> SetCorporateEntityAsAuthor()
+    {
+        authorCorporate = new()
+        {
+            isMainEntry = false,
+            entryType = 2,
+            FirstName = "My Company Inc."
+        };
+
+        return authorCorporate;
+    }
+}
