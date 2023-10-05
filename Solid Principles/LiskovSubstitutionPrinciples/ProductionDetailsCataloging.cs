e40aa91981b7ef6080ce266a710af023826c7fe7
Bryan Jerahmeel Yao <bgyao@live.com>
2023-10-05 16:57:15 +0800

    Application of LSP on MARC 264

---
+++ Solid Principles/LiskovSubstitutionPrinciples/ProductionDetailsCataloging.cs
@@ -0,0 +1,29 @@
+﻿using Solid_Principles.LiskovSubstitutionPrinciples.PublicationDetails;
+
+namespace Solid_Principles.LiskovSubstitutionPrinciples;
+
+public class ProductionDetailsCataloging
+{
+    public static void Main()
+    {
+        var copyrightDetail = new CopyrightDetail()
+        {
+            CopyrightHolder = "HarperCollins",
+            CopyrightPlace = "New York, NY",
+            CopyrightDate = new DateOnly(2022, 10, 30)
+        };
+
+        var publisherDetail = new PublisherDetail()
+        {
+            PublicationPlace = "Washington, DC",
+            PublisherName = "Penguin Publication",
+            PublicationDate = new DateOnly(2023, 1, 15)
+        };
+
+        copyrightDetail.PrintPublicationDetails();
+        publisherDetail.PrintPublicationDetails();
+
+        Console.ReadKey();
+    }
+    
+}
---
+++ Solid Principles/LiskovSubstitutionPrinciples/PublicationDetails/PublisherDetail.cs
@@ -0,0 +1,34 @@
+﻿namespace Solid_Principles.LiskovSubstitutionPrinciples.PublicationDetails;
+
+// Based on MARC 264 - Production, Publication, Distribution, Manufacture, and Copyright Notice (R)
+// For this exercise, LSP will be demonstrated by using PublicationDetail as the abstract class,
+// then PublisherDetail (2nd Indicator = 1) and CopyrightDetail (2nd Indicator = 4)
+// will inherit the PublicationDetail.
+public class PublisherDetail : PublicationDetail
+{
+    public string? PublicationPlace { get; set; }
+    public string? PublisherName { get; set; }
+    public DateOnly PublicationDate { get; set; }
+    private string PublicationYear = "[year of publication not identified]";
+    public string? MaterialsSpecified { get; set; }
+    public string? Linkage { get; set; }
+    public Guid DataProvenance { get { return new Guid(); } }
+    public Guid SequenceNumber { get { return new Guid(); } }
+
+    public override void PrintPublicationDetails()
+    {
+        PublicationPlace ??= "[place of publication not identified]";
+
+        PublisherName ??= "[publisher not identified]";
+
+        if(PublicationDate.ToString() is not null)
+        {
+            Console.WriteLine($"264 #1 $a{PublicationPlace} : $b{PublisherName}, $c{PublicationDate.Year}.");
+        }
+        else
+        {
+            Console.WriteLine($"264 #1 $a{PublicationPlace} : $b{PublisherName}, $c{PublicationYear}.");
+        }
+        
+    }
+}
