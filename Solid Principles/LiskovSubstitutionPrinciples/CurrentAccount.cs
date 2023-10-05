fadb7e3401860c619d583f5dfbe3cbffc9cb221e
Bryan Jerahmeel Yao <bgyao@live.com>
2023-10-05 10:45:50 +0800

    Liskov Substitution Principle
    The Liskov Substitution Principle says that the object of a derived class should be able to replace an object of the base class without bringing any errors in the system or modifying the behavior of the base class. That means child class objects should be able to replace parent class objects without compromising application integrity.
    
    Source: https://dotnettutorials.net/lesson/liskov-substitution-principle/

---
+++ Solid Principles/LiskovSubstitutionPrinciples/CurrentAccount.cs
@@ -0,0 +1,24 @@
+﻿namespace Solid_Principles.LiskovSubstitutionPrinciples;
+
+public class CurrentAccount : BankAccount
+{
+    public decimal OverdraftLimit { get; set; }
+
+    public CurrentAccount(string accountNum, decimal balance, decimal overdraftLimit) : base (accountNum, balance)
+    {
+        OverdraftLimit = overdraftLimit;
+    }
+
+    public override void Withdraw(decimal amount)
+    {
+        if(amount <= Balance + OverdraftLimit)
+        {
+            Balance -= amount;
+            Console.WriteLine($"Withdraw of {amount} from Account Number {AccountNum} successful. New Bal: {Balance}");
+        }
+        else
+        {
+            Console.WriteLine($"Exceeded Overdraft Limit or low balance. Account Number: {AccountNum} Current Bal: {Balance}");
+        }
+    }
+}
---
+++ Solid Principles/LiskovSubstitutionPrinciples/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Solid_Principles.LiskovSubstitutionPrinciples;
+
+public class Program
+{
+    public static void Main()
+    {
+        BankAccount savingsAccount = new SavingsAccount("SA123456789", 1000m, 0.05m);
+        BankAccount currentAccount = new CurrentAccount("CA987654321", 1500m, 500m);
+
+        Console.WriteLine("Before Transactions:");
+        PrintAccountDetails(savingsAccount);
+        PrintAccountDetails(currentAccount);
+
+        savingsAccount.Withdraw(500m);
+        currentAccount.Withdraw(2000m);
+
+        Console.WriteLine("After Transactions:");
+        PrintAccountDetails(savingsAccount);
+        PrintAccountDetails(currentAccount);
+    }
+
+    static void PrintAccountDetails(BankAccount bankAccount)
+    {
+        Console.WriteLine($"Account Number: {bankAccount.AccountNum} Balance {bankAccount.Balance}");
+    }
+}
