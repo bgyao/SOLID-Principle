855541c07aa119c7836fb4d95cb2628ce128bc99
Bryan Jerahmeel Yao <bgyao@live.com>
2023-10-12 16:38:19 +0800

    Single Responsibility Principle hotfix
    - Separated BookDetails and Author into two distinct .cs files
    - Added Program.cs

---
+++ Solid Principles/SingleResponsibilityPrinciples/DescriptiveCatalogings/BookDetails.cs
@@ -0,0 +1,22 @@
+﻿namespace Solid_Principles.SingleResponsibilityPrinciples.DescriptiveCatalogings;
+
+public class BookDetails
+{
+    public Guid id = new Guid();
+    public string bookTitle;
+    public string? bookSubtitle;
+    public string? edition;
+
+    public void PrintBookBasicDetails()
+    {
+        var displayDetails = $"This book is titled {bookTitle}";
+
+        if (!string.IsNullOrEmpty(bookSubtitle))
+        {
+            displayDetails = string.Join(" : ", displayDetails, bookSubtitle);
+        }
+
+        displayDetails = string.Join(", ", displayDetails, edition);
+        Console.WriteLine(displayDetails);
+    }
+}
